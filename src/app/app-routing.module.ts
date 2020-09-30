f435593d959f957c82ad39c7294c02e198f9f7af
Justyna <justyna.zbyszynska@gmail.com>
2020-09-30 23:43:22 +0200

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {AboutComponent} from "./component/about/about.component";
+import {ContactComponent} from "./component/contact/contact.component";
+import {HomeComponent} from "./component/home/home.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  }
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
